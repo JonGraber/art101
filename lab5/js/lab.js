7c4611e1a0e925d909bcc431c0a2aebcbcbed230
JonGraber <91684925+JonGraber@users.noreply.github.com>
2021-10-17 22:22:24 -0700

    lab5 no1

---
+++ lab5/js/lab.js
@@ -0,0 +1,26 @@
+/**
+ * Author:    Name
+ * Created:   11.05.2009
+ *
+ * (c) Copyright by Blub Corp.
+ **/
+
+ // Define Variables
+ make = "Ford";
+model = "Focus";
+color = "Blue";
+year = "2013";
+
+//Calculate
+age = 2021 - year;
+
+//Boolean
+var ownit = true
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("color: " + color + "<br>");
+document.writeln("year: " + year + "<br>");
+document.writeln("age: " + age + "years<br>");
+document.writeln("ownit: " + ownit + "<br>");
