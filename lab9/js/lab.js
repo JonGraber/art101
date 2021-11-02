d14cfba332a78b0b37a7adb0a3d82455e521d552
JonGraber <91684925+JonGraber@users.noreply.github.com>
2021-11-01 21:36:47 -0700

    lab9

---
+++ lab9/js/lab.js
@@ -0,0 +1,26 @@
+/**
+ * Author:    Name
+ * Created:   11.05.2009
+ *
+ * (c) Copyright by Blub Corp.
+ **/
+
+ // Use getElementById() to find your output <div> and assign it to a variable outputEl
+
+document.getElementById('output')
+document.getElementById('output') = var outputEl
+outputEL.innerHTML = "<button id=mybutton>Press this Button!</button>";
+
+ var new1El = document.createElement("button2")
+
+ new1El.innerHTML = "Don't press this one";
+
+ outputEl.appendChild(new1El)
+ // Create a new element with document.createElement("p") and assign it to a variable new1El
+
+
+ // Change the html attribute of new1El to say something new.
+ // Create another new element and assign it to a variable new2El
+ // Change the html attribute of new2El to say something else.
+ // Append both new elements one at a time using appendChild() to your output <div>
+ // Change the css attributes of at least two elements of your page
