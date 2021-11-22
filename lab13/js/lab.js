88cac7510958335791cead45b7f96eb7a5d07f4c
JonGraber <91684925+JonGraber@users.noreply.github.com>
2021-11-22 11:29:30 -0800

    13

---
+++ lab13/js/lab.js
@@ -0,0 +1,35 @@
+
+
+
+
+for (var i=0; i<=200; i++) {
+  var str= "";
+  if (i % 3 == 0) {
+    str += "Fizz";
+  }
+  if (i % 5 == 0) {
+    str += "Buzz";
+  }
+  if (i % 7 == 0) {
+    str += "Boom";
+  }
+  if (i % 13 == 0) {
+    str += "Bang";
+  }
+  if (str == "") {
+    str = i;
+  }
+  else {
+    str += "!";
+  }
+  console.log(str);
+}
+
+str += "Fizz";
+$("#output").append("<p>" + str + "</p>");
+str += "Buzz";
+$("#output").append("<p>" + str + "</p>");
+str += "Boom";
+$("#output").append("<p>" + str + "</p>");
+str += "Bang";
+$("#output").append("<p>" + str + "</p>");
